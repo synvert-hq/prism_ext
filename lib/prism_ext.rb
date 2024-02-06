057d3f73969b760fee8f36c15cdd24a6e26404e0
Richard Huang <flyerhzm@gmail.com>
2024-02-06 14:38:33 +0800

    initial commit

---
+++ lib/prism_ext.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "prism_ext/version"
+
+module PrismExt
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ spec/prism_ext_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe PrismExt do
+  it "has a version number" do
+    expect(PrismExt::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
