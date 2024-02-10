47f6c4afc7ec0d848623cf56b5a434fac37ddb51
Richard Huang <flyerhzm@gmail.com>
2024-02-10 09:59:30 +0800

    add keys, values, hash_pair and hash_value methods to hash node

--- lib/prism_ext.rb
+++ lib/prism_ext.rb
@@ -13,5 +13,37 @@ module Prism
     def source
       location.instance_variable_get(:@source).source
     end
+
+    def keys
+      if is_a?(HashNode)
+        elements.map(&:key)
+      else
+        raise MethodNotSupported, "keys is not supported for #{self}"
+      end
+    end
+
+    def values
+      if is_a?(HashNode)
+        elements.map(&:value)
+      else
+        raise MethodNotSupported, "values is not supported for #{self}"
+      end
+    end
+
+    def hash_pair(key)
+      if is_a?(HashNode)
+        elements.find { |element_node| element_node.key.to_value == key }
+      else
+        raise MethodNotSupported, "hash_pair is not supported for #{self}"
+      end
+    end
+
+    def hash_value(key)
+      if is_a?(HashNode)
+        elements.find { |element_node| element_node.key.to_value == key }&.value
+      else
+        raise MethodNotSupported, "hash_value is not supported for #{self}"
+      end
+    end
   end
 end
--- spec/prism_ext_spec.rb
+++ spec/prism_ext_spec.rb
@@ -20,4 +20,42 @@ RSpec.describe PrismExt do
   it 'gets source' do
     expect(node.source).to eq source
   end
+
+  describe '#keys' do
+    it 'gets for hash node' do
+      node = parse("{:foo => :bar, 'foo' => 'bar'}")
+      expect(node.keys).to eq [node.elements[0].key, node.elements[1].key]
+    end
+  end
+
+  describe '#values' do
+    it 'gets for hash node' do
+      node = parse("{:foo => :bar, 'foo' => 'bar'}")
+      expect(node.values).to eq [node.elements[0].value, node.elements[1].value]
+    end
+  end
+
+  describe '#hash_pair' do
+    it 'gets pair of specified key' do
+      node = parse('{:foo => :bar}')
+      expect(node.hash_pair(:foo)).to eq node.elements[0]
+    end
+
+    it 'gets nil if key does not exist' do
+      node = parse('{:foo => :bar}')
+      expect(node.hash_pair(:bar)).to be_nil
+    end
+  end
+
+  describe '#hash_value' do
+    it 'gets value of specified key' do
+      node = parse('{:foo => :bar}')
+      expect(node.hash_value(:foo).to_value).to eq :bar
+    end
+
+    it 'gets nil if key does not exist' do
+      node = parse('{:foo => :bar}')
+      expect(node.hash_value(:bar)).to be_nil
+    end
+  end
 end
